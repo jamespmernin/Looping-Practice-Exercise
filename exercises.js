d55492432b535c91df9734b668ec1717fd534ddc
James Mernin <jamespmernin@gmail.com>
2020-09-10 14:20:33 -0400

    Completed Exxercise 1

--- exercises.js
+++ exercises.js
@@ -1,8 +1,24 @@
 //Exercise 1
+let temp = 200;
+if (temp <= 32) {
+  console.log("Watch out for ice.");
+} else if (temp <= 55) {
+  console.log("Better put on a jacket.");
+} else if (temp <= 70) {
+  console.log("Such nice weather we're having.");
+} else if (temp <= 99) {
+  console.log("Summer is here.");
+} else if (temp === 212 || temp === 444) {
+  console.log("Where are you exactly?");
+} else {
+  console.log("Global warming!");
+}
 
 //Exercise 2
 
+
 //Exercise 3
+
 
 //Exercise 4
 
